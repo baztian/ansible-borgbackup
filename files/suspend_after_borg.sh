ead52fd0caf9c648c9059f2085d683d4adc3b790
baztian <bastian.dev@gmail.com>
2025-02-05 08:19:01 +0100

    Add timestamps to suspend script

--- files/suspend_after_borg.sh
+++ files/suspend_after_borg.sh
@@ -5,12 +5,16 @@ is_borg_running() {
     pgrep -x "borg" > /dev/null
 }
 
-echo "Waiting for Borg to finish..."
+print_timestamp() {
+    date '+%Y-%m-%d %H:%M:%S'
+}
+
+echo "$(print_timestamp) - Waiting for Borg to finish..."
 
 # Wait for Borg to finish
 while is_borg_running; do
     sleep 30  # Check every 30 seconds
 done
 
-echo "Borg backup completed. Suspending now..."
+echo "$(print_timestamp) - Borg backup completed. Suspending now..."
 systemctl suspend
